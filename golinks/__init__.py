db5942f4dfccda0107ac98924196c0d93d07a8f9
Richy <redluxx@hydramail.co.uk>
2020-07-16 18:33:05 +0100

    flip slash

--- golinks/__init__.py
+++ golinks/__init__.py
@@ -7,7 +7,7 @@ import os
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, template_folder="templates")
-    app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///' + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db\db.sqlite')
+    app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///' + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db/db.sqlite')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'CHANGE_ME'
     app.debug = False
